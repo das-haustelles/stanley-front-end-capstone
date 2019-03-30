cf104874016889a9d13ec4ef62d7bf2357d87899
Stanley Wu <stanleychuwu@gmail.com>
2019-03-29 17:40:29 -0700

    Update branch with newest changes

---
+++ database/index.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+// mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/searchBar');
+var searchDB = mongoose.connection;
+
+//search input schema
+var searchInputSchema = mongoose.Schema({
+    name: String,
+    city: String,
+});
+
+var SearchInputModel = mongoose.model('Input', searchInputSchema);
+
+//check in check out schema
+var searchCalendarSchema = mongoose.Schema({
+    unavailable: Number,
+})
+
+var CalendarModel = mongoose.model('Calendar', searchCalendarSchema);
+
+//groups schema
+var searchGroupsSchema = mongoose.Schema({
+    groups: Number,
+})
+
+var GroupsModel = mongoose.model('Groups', searchGroupsSchema);
+
+
+
+searchDB.on('error', console.error.bind(console, 'connection error:'));
+searchDB.once('open', function() {
+  console.log('database connected fam');
+});
+
+module.exports = {
+    searchDB: searchDB, 
+    SearchInputModel: SearchInputModel, 
+    CalendarModel: CalendarModel, 
+    GroupsModel: GroupsModel,
+}
