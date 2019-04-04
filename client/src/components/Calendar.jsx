72602600cefd80b835b4ed9bbd8bcc46b54c2085
Stanley Wu <stanleychuwu@gmail.com>
2019-04-04 15:49:30 -0700

    Update Date's State dynamically

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -8,44 +8,37 @@ class Calendar extends React.Component {
       firstDay: '',
     }
   }
-  //--------------------------------------------------------
-  // IMPLEMENTATION
-  //--------------------------------------------------------
-  // we need to set a new Date with a getMonth method
-
-  componentDidMount() {
-    var day = new Date();
-    day.setDate(1);
-    console.log('currentDay = ', day);
-    //Calendar's state needs to be the first day of the month
-    //still need to figure out what happens on click for next month
-    //onClick will change state to increment month and possibly year
-    this.setState({firstDay: day});
-  }
-
   weekDays() {
     return (
       <div>
+        <span> S </span>
         <span> M </span>
         <span> T </span>
         <span> W </span>
         <span> T </span>
         <span> F </span>
         <span> S </span>
-        <span> S </span>
       </div>
     )
   }
-  
-  firstDayOfMonth() {
-    var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var weekday = new Date();
-    return weekdays[weekday.getDay()];
+  //PROBLEM IS HERE!!!!!!!!!!!!!!!!!!
+  changeStateToFirst() {
+    var day = new Date();
+    day.setDate(1);
+    this.state.firstDay = day;
+    console.log("this.state.firstDay = ", this.state.firstDay)
   }
+
+  //NOTE: might not need these lines of code. Will delete if not needed
+  // firstDayOfMonth() {
+  //   var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  //   var weekday = new Date();
+  //   return weekdays[weekday.getDay()];
+  // }
   
   render() {
     return (
-      <div>
+      <div>{this.changeStateToFirst()}
         <div>{this.weekDays()}</div>
         <Dates firstday={this.state.firstDay}/>
       </div>
--- client/src/components/Dates.jsx
+++ client/src/components/Dates.jsx
@@ -3,8 +3,26 @@
 class Dates extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      day: '',
+    }
   }
+
+  //---CURRENT IDEA----
+  //we're gonna setState to firstDay - getDay. 
+  //we'll set the key to the date...?
+    //can't do that because there might be two 1st, or two 29th's
+  firstDayOfMonth() {
+    console.log("this.props.firstday = ", this.props.firstday)
+    var firstOfMonth = this.props.firstday;
+    firstOfMonth.setDate(firstOfMonth.getDate() - firstOfMonth.getDay())
+    this.state.day = firstOfMonth;
+    console.log("this.state.day = ", this.state.day)
+  }
+
+  //dateMaker makes the Dates in the calendar
   dateMaker() {
+    console.log('props in Dates has ', this.props)
     let dates = [];
     for (var i = 0; i < 6; i++) {
       dates.push(<div></div>)
@@ -18,7 +36,7 @@ class Dates extends React.Component {
 
   render() {
     return (
-      <div>
+      <div>{this.firstDayOfMonth()}
         <div>{this.dateMaker()}</div>
       </div>
     )
