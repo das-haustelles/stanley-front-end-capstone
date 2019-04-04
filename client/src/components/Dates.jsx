4c6d364aef1c32a78b8aad6c27b938160c218be1
Stanley Wu <stanleychuwu@gmail.com>
2019-04-04 16:02:31 -0700

    Render calendar dynamically

--- client/src/components/Dates.jsx
+++ client/src/components/Dates.jsx
@@ -13,22 +13,22 @@ class Dates extends React.Component {
   //we'll set the key to the date...?
     //can't do that because there might be two 1st, or two 29th's
   firstDayOfMonth() {
-    console.log("this.props.firstday = ", this.props.firstday)
     var firstOfMonth = this.props.firstday;
     firstOfMonth.setDate(firstOfMonth.getDate() - firstOfMonth.getDay())
     this.state.day = firstOfMonth;
-    console.log("this.state.day = ", this.state.day)
   }
 
   //dateMaker makes the Dates in the calendar
   dateMaker() {
-    console.log('props in Dates has ', this.props)
     let dates = [];
     for (var i = 0; i < 6; i++) {
       dates.push(<div></div>)
       for (var j = 0; j < 7; j++) {
+        var calendarDate = this.state.day;
+        console.log("calendarDate = ", calendarDate);
         //key will give coordinates to each date
-        dates.push(<span key={i.toString() + j.toString()}> Date </span>)
+        dates.push(<span key={calendarDate}> {calendarDate.getDate()} </span>)
+        calendarDate.setDate(calendarDate.getDate() + 1);
       }
     }
     return dates;
